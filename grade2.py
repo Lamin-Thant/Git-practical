ba799af4dcf7fd6eee7796476a4178c422e98520
LMT <clearmoon378@gmail.com>
2026-09-15 13:37:55 +0630

    add grade2.py from local

---
+++ grade2.py
@@ -0,0 +1,17 @@
+student_name = input("Enter student name: ")
+
+first_mark = float(input("Enter first subject mark: "))
+second_mark = float(input("Enter second subject mark: "))
+
+total_mark = first_mark + second_mark
+average_mark = total_mark/2
+
+if average_mark >= 50:
+    result = "Pass"
+else:
+    result = "Fail"
+
+print("\nStudent Name:", student_name)
+print("Total Mark:",total_mark)
+print("Average Mark:", int(average_mark))
+print("Result:",result)
